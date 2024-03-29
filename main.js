6bec22c7f1d9d7a3caaeac281bc74bcfb73a72cf
Erika2106 <105748835+Erika2106@users.noreply.github.com>
2024-03-29 14:40:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,19 +15,12 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-11-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-08-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-
-function atualizarCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-    
-}
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -40,6 +33,28 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
+
+function atualizaCronometro(){
+
+    for (let i=0; i<contadores.length;i++){
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+    
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
